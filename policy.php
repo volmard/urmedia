6e6b64a47a86ce593794132fd5aaa9ccac04648e
Volodymyr Umanets <vladimirumanets@gmail.com>
2017-04-11 00:30:37 +0200

    Update policy.php

--- policy.php
+++ policy.php
@@ -1,40 +1,78 @@
-<main class="content">
-	<section class="content__section">
-		<div class="content__container">
-			<h3 class="content__title">
-				What is this Privacy Policy for?
-			</h3>
-			<p class="content__paragraph">
-				This privacy policy is for this website and served by URMEDIA and governs the privacy of its users who choose to use it.
-			</p>
-			<p class="content__paragraph">	
-				The policy sets out the different areas where user privacy is concerned and outlines the obligations and requirements of the users, the website and website owners. Furthermore the way this website processes, stores and protects user data and information will also be detailed within this policy.
-			</p>
-			<h3 class="content__title">
-				The Website
-			</h3>
-			<p class="content__paragraph">
-				This website and its owners take a proactive approach to user privacy and ensure the necessary steps are taken to protect the privacy of its users throughout their visiting experience. This website complies to all UK national laws and requirements for user privacy.
-			</p>
-			<h3 class="content__title">
-				Use of Cookies
-			</h3>
-			<p class="content__paragraph">
-				This website uses cookies to better the users experience while visiting the website. Where applicable this website uses a cookie control system allowing the user on their first visit to the website to allow or disallow the use of cookies on their computer / device. This complies with recent legislation requirements for websites to obtain explicit consent from users before leaving behind or reading files such as cookies on a user's computer / device.
-			</p> 
-			<p class="content__paragraph">	
-				Cookies are small files saved to the user's computers hard drive that track, save and store information about the user's interactions and usage of the website. This allows the website, through its server to provide the users with a tailored experience within this website.
-			</p> 
-			<p class="content__paragraph">	
-				Users are advised that if they wish to deny the use and saving of cookies from this website on to their computers hard drive they should take necessary steps within their web browsers security settings to block all cookies from this website and its external serving vendors.
-			</p> 
-			<p class="content__paragraph">	
-				This website uses tracking software to monitor its visitors to better understand how they use it. This software is provided by Google Analytics which uses cookies to track visitor usage. The software will save a cookie to your computers hard drive in order to track and monitor your engagement and usage of the website, but will not store, save or collect personal information. You can read Google's privacy policy here for further information [ http://www.google.com/privacy.html ].
-			</p> 
-			<p class="content__paragraph">	
-				Other cookies may be stored to your computers hard drive by external vendors when this website uses referral programs, sponsored links or adverts. Such cookies are used for conversion and referral tracking and typically expire after 30 days, though some may take longer. No personal information is stored, saved or collected.
-			</p> 
-		</div>
-  </section>   
-</main>
+<main class="content">
+
+	<section class="content__section">
+
+		<div class="content__container">
+
+			<h3 class="content__title">
+
+				What is this Privacy Policy for?
+
+			</h3>
+
+			<p class="content__paragraph">
+
+				This privacy policy is for this website and served by URMEDIA and governs the privacy of its users who choose to use it.
+
+			</p>
+
+			<p class="content__paragraph">	
+
+				The policy sets out the different areas where user privacy is concerned and outlines the obligations and requirements of the users, the website and website owners. Furthermore the way this website processes, stores and protects user data and information will also be detailed within this policy.
+
+			</p>
+
+			<h3 class="content__title">
+
+				The Website
+
+			</h3>
+
+			<p class="content__paragraph">
+
+				This website and its owners take a proactive approach to user privacy and ensure the necessary steps are taken to protect the privacy of its users throughout their visiting experience. This website complies to all UK national laws and requirements for user privacy.
+
+			</p>
+
+			<h3 class="content__title">
+
+				Use of Cookies
+
+			</h3>
+
+			<p class="content__paragraph">
+
+				This website uses cookies to better the users experience while visiting the website. Where applicable this website uses a cookie control system allowing the user on their first visit to the website to allow or disallow the use of cookies on their computer / device. This complies with recent legislation requirements for websites to obtain explicit consent from users before leaving behind or reading files such as cookies on a user's computer / device.
+
+			</p> 
+
+			<p class="content__paragraph">	
+
+				Cookies are small files saved to the user's computers hard drive that track, save and store information about the user's interactions and usage of the website. This allows the website, through its server to provide the users with a tailored experience within this website.
+
+			</p> 
+
+			<p class="content__paragraph">	
+
+				Users are advised that if they wish to deny the use and saving of cookies from this website on to their computers hard drive they should take necessary steps within their web browsers security settings to block all cookies from this website and its external serving vendors.
+
+			</p> 
+
+			<p class="content__paragraph">	
+
+				This website uses tracking software to monitor its visitors to better understand how they use it. This software is provided by Google Analytics which uses cookies to track visitor usage. The software will save a cookie to your computers hard drive in order to track and monitor your engagement and usage of the website, but will not store, save or collect personal information. You can read Google's privacy policy here for further information [ http://www.google.com/privacy.html ].
+
+			</p> 
+
+			<p class="content__paragraph">	
+
+				Other cookies may be stored to your computers hard drive by external vendors when this website uses referral programs, sponsored links or adverts. Such cookies are used for conversion and referral tracking and typically expire after 30 days, though some may take longer. No personal information is stored, saved or collected.
+
+			</p> 
+
+		</div>
+
+  </section>   
+
+</main>
 <!--http://jamieking.co.uk/resources/download-free-sample-privacy-policy.html-->
