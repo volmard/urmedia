3c6ef1b9142872f0f6f27f29859a109f73f6343d
Volodymyr Umanets <vladimirumanets@gmail.com>
2017-04-10 23:54:56 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,48 +1,94 @@
-<?php require_once("includes/layouts/header.php");?>
-<?php
-  $id=set_ids();
-  switch($id){ 
-    case 'item': 
-      include 'item.php'; 
-    break;
-    case 'catalog': 
-      include 'catalog.php'; 
-    break;
-    case 'search': 
-      include 'search.php';                
-    break;
-		case 'brands': 
-      include 'brands.php';                
-    break;
-		case 'cart': 
-      include 'cart.php';                
-    break;
-		case 'orderform': 
-      include 'orderform.php';                
-    break;
-		case 'orders': 
-      include 'orders.php';                
-    break;
-		case 'contacts': 
-      include 'contacts.php';                
-    break;
-		case 'news': 
-      include 'news.php';                
-    break;
-		case 'about': 
-      include 'about.php';                
-    break;
-		case 'faq': 
-      include 'faq.php';                
-    break;
-		case 'policy': 
-      include 'policy.php';                
-    break;
-		case 'termsofservice': 
-      include 'terms_of_service.php';                
-    break;
-    default:  
-      include 'main.php';    
-  }
-?>
+<?php require_once("includes/layouts/header.php");?>
+
+<?php
+
+  $id=set_ids();
+
+  switch($id){ 
+
+    case 'item': 
+
+      include 'item.php'; 
+
+    break;
+
+    case 'catalog': 
+
+      include 'catalog.php'; 
+
+    break;
+
+    case 'search': 
+
+      include 'search.php';                
+
+    break;
+
+		case 'brands': 
+
+      include 'brands.php';                
+
+    break;
+
+		case 'cart': 
+
+      include 'cart.php';                
+
+    break;
+
+		case 'orderform': 
+
+      include 'orderform.php';                
+
+    break;
+
+		case 'orders': 
+
+      include 'orders.php';                
+
+    break;
+
+		case 'contacts': 
+
+      include 'contacts.php';                
+
+    break;
+
+		case 'news': 
+
+      include 'news.php';                
+
+    break;
+
+		case 'about': 
+
+      include 'about.php';                
+
+    break;
+
+		case 'faq': 
+
+      include 'faq.php';                
+
+    break;
+
+		case 'policy': 
+
+      include 'policy.php';                
+
+    break;
+
+		case 'termsofservice': 
+
+      include 'terms_of_service.php';                
+
+    break;
+
+    default:  
+
+      include 'main.php';    
+
+  }
+
+?>
 <?php require_once("includes/layouts/footer.php");?>
